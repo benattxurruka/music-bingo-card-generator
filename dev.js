ffd85eafb62bc241661364df2568f7981d01f89f
Beñat Txurruka <betxurruka@gmail.com>
2026-05-11 18:20:43 +0200

    Add logo and favicon.

--- dev.js
+++ dev.js
@@ -19,12 +19,41 @@ try {
   });
 } catch (_) { /* .env is optional */ }
 
+const path = require('path');
+
 const port = Number(process.argv[2]) || 3000;
 
 const html = fs.readFileSync('index.html', 'utf8')
   .replace(/__SPOTIFY_CLIENT_ID__/g, process.env.SPOTIFY_CLIENT_ID || '');
 
+const MIME = {
+  '.html': 'text/html; charset=utf-8',
+  '.js':   'application/javascript',
+  '.png':  'image/png',
+  '.jpg':  'image/jpeg',
+  '.ico':  'image/x-icon',
+  '.svg':  'image/svg+xml',
+  '.css':  'text/css',
+};
+
 http.createServer((req, res) => {
+  const url = req.url.split('?')[0];
+
+  // Serve index.html for root
+  if (url === '/' || url === '/index.html') {
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    return res.end(html);
+  }
+
+  // Try to serve static files from project root
+  const filePath = path.join(__dirname, url);
+  const ext = path.extname(filePath);
+  if (MIME[ext] && fs.existsSync(filePath)) {
+    res.writeHead(200, { 'Content-Type': MIME[ext] });
+    return res.end(fs.readFileSync(filePath));
+  }
+
+  // Fallback to index.html
   res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
   res.end(html);
 }).listen(port, () => {
